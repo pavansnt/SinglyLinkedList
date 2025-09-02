e69fdd08938b13f6b3729007690172e203fb5fd8
pavansnt <pavansnnt55@gmail.com>
2025-09-02 12:36:07 +0530

    #

--- Node.java
+++ Node.java
@@ -5,6 +5,6 @@ public class Node{
     public Node(int data){
         this.data = data;
         this.next = null;
-        System.out.println("New Node created with data = " + data);
+        System.out.println("New  node created with data = " + data);
     }
 }
