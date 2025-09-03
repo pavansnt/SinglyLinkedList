a685af49eb28ebe4500e5a83eb0b3ca5265a7360
pavansnt <pavansnnt55@gmail.com>
2025-09-03 12:26:46 +0530

    Added some othe functions

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -58,30 +58,103 @@ public class SinglyLinkedList {
             currentNode = currentNode.next;
             currentPosition++;
         }
-        Node newNode = new Node(data);
-        newNode.next = currentNode.next;
-        currentNode.next = newNode;
-
+        
         if (currentNode == null) {
             System.err.println("Invalid position! Out of range.");
             return;
         }
+
+        Node newNode = new Node(data);
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+
+        
+    }
+
+    public void deleteAtBeginning(){
+        //list is empty
+        if(head == null){
+            System.out.println("Nothing to delete since list empty");
+            return;
+        }
+
+        //if list has  one or more  node
+        head = head.next;
+        System.out.println("First node deleted");
+
+    }
+
+    public void deleteAtEnd(){
+        //if list is empty
+        if(head == null){
+            System.out.println("Nothing to delete since list empty");
+            return;
+        }
+
+        //if list has only one node
+        if(head.next == null){
+            head = null;
+            System.out.println("Delted that only one node which is in the list");
+            return;
+        } 
+
+        Node currentNode = this.head;
+       // while(currentNode != null)  --> ❌ here we will reach end of the list till null
+       // while(currentNode.next != null) --> ❌ here we reach tp last node which we need to delete
+        while(currentNode.next.next != null){ // ---> ✅ here we reach the one node of  before the last node
+            currentNode = currentNode.next;
+        }
+        currentNode.next = null;
+        System.out.println("Deletd node at End of List");
+    }
+
+    public void SearchNode(int data){
+
+        if(this.head == null){
+            System.out.println("List is Empty");
+        }
+
+        if(data <= 0){
+            System.out.println("Invalid Search!!");
+        }
+
+        Node currentNode = this.head;
+        int currentPosition = 1;
+
+        while(currentNode.next != null){
+            if(currentNode.data == data){
+                System.out.println("given Node = "+ data +" found at position : " + currentPosition);
+                //return currentPosition;
+            }
+            currentNode = currentNode.next;
+        }
+        System.out.println(" Node = " +data+ " Not found!!");
+        //return -1;
     }
 
     public void testMethod(){
+
         insertAtBeginnning(1);  // List: 1
         insertAtBeginnning(2);  // List: 2 -> 1
         insertAtPosition(3, 3);
-        insertAtPosition(4, 5);
-        // insertAtPosition(1,-1);
-        // insertAtPosition(1,0);
-
-        // insertAtBeginnning(1);
-        // insertAtPosition(2,1);
-
-        // insertAtPosition(3,3);
-        // insertAtPosition(10,4);
+        //insertAtPosition(4, 5); this wont work it will print "invalid postion! Out of range"
         printAllOperations();
+
+        System.out.println("-------After deletion------");
+        deleteAtBeginning();
+        deleteAtEnd();
+        printAllOperations();
+        SearchNode(2);
+
+        //below code can used when we need to return value from search method
+        // int result = SearchNode(2);
+        // if(result != -1){
+        //      System.out.println("given Node = "+ data +" found at position : " + result);
+        // }
+        // System.out.println(" Node = " +data+ " Not found!!");
+        
+
+
     }
 
     public void printAllOperations()
